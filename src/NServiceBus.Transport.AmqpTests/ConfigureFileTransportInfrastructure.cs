5bc5db32099505d909088913d6905ed854ea54a7
m-monaghan <65737446+m-monaghan@users.noreply.github.com>
2020-05-31 09:32:55 +0100

    Create a unit test (NUnit) project

---
+++ src/NServiceBus.Transport.AmqpTests/ConfigureFileTransportInfrastructure.cs
@@ -0,0 +1,14 @@
+namespace NServiceBus.Transport.AmqpTests {
+    using NUnit.Framework;
+
+    public class ConfigureFileTransportInfrastructure {
+        [SetUp]
+        public void Setup () {
+        }
+
+        [Test]
+        public void Test1 () {
+            Assert.Pass ();
+        }
+    }
+}
